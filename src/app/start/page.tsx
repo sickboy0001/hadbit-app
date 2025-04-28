fb2e4713d8a12a5619f5872ee0029b213ed9dfc5
tfujiki <syunjyu0001@gmail.com>
2025-04-29 08:55:19 +0900

    Initial commit of Hadbit MVP

---
+++ src/app/start/page.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hadbit MVP</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Welcome to Hadbit MVP!</h1>
+        <p>This is a minimal viable product built with Next.js.</p>
+      </main>
+
+      <footer>
+        <p>&copy; 2025 Hadbit</p>
+      </footer>
+    </div>
+  );
+}
